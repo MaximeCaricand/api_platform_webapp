03241762a04ae7f28e02aba7a4c74e8852b52b04
Niels Kerné <365-kn175318@users.noreply.www-apps.univ-lehavre.fr>
2021-12-22 22:46:06 +0100

    création react component

--- pwa/pages/PrixMetre2ParAnnee.tsx
+++ pwa/pages/PrixMetre2ParAnnee.tsx
@@ -1,5 +1,17 @@
-const fakeData = { "2017" : 1, "2018" : 2, "2019": 3 };
+import { FunctionComponent, useState, useEffect } from "react";
 
+
+const fakeData = {0 : {"date" : "2017", "prixMoyenParMettreCarre": 2}, 1 : {"date" : "2018", "prixMoyenParMettreCarre": 3}, 2 : {"date" : "2019", "prixMoyenParMettreCarre": 4}};
+
+// dans le conteneur React
 export default function PrixMetre2ParAnneeComponnent() {
-    return <div id="graphiquePrixMoyenParMCarre"></div>
+    useEffect(() => {
+
+    }, [])
+
+//...
+    return (
+        //...
+        <svg></svg>
+    )
 }
