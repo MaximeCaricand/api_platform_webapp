459859bbc2bde749cf3dfe31b5e2b3b393cd9d08
ls150611 <solenne.lebrun@etu.univ-lehavre.fr>
2021-12-22 22:23:59 +0100

    Chose étrange

--- pwa/pages/sales-per-area.tsx
+++ pwa/pages/sales-per-area.tsx
@@ -1,12 +1,94 @@
-const Index = () => (
-    <>
-        <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-12 p-5 text-center">
-                    <h2 className="fw-bold mb-5">Ventes par région</h2>
-                </div>
-            </div>
+import d3, { svg } from "d3";
+import Head from "next/head";
+import { FunctionComponent, useState, useEffect, useRef } from "react";
+
+const donut_chart = ({donut_data}) => (
+  
+  <>
+    <Head>
+      <title>FullStack Lab</title>
+    </Head>
+
+    <div>
+      <header>
+      </header>
+      <section>
+        <div>
+          <h1>
+            <strong>Sale distribution per region</strong>
+          </h1>
         </div>
-    </>
+      </section>
+    </div>
+
+    <ul>
+      {donut_data["hydra:member"].map((obj) => (
+        <li>{obj.region + " " + obj.distribution}</li>
+      ))}
+    </ul>
+
+  </>
 );
-export default Index;
+
+const Donut_chart = ({donut_data}) => {
+  const ref = useRef()
+  useEffect(() => {
+    async function fetchD3(){
+
+        const d3:any = await import('d3') // asynchronically import d3 
+        const svg = d3.select('svg');
+    
+      
+        
+    }
+    fetchD3()
+  }, [])
+
+  return (    
+    <>
+      <Head>
+        <title>FullStack Lab</title>
+      </Head>
+
+      <div>
+        <header>
+        </header>
+        <section>
+          <div>
+            <h1>
+              <strong>Sale distribution per region</strong>
+            </h1>
+          </div>
+        </section>
+      </div>
+
+      <ul>
+        {donut_data["hydra:member"].map((obj) => (
+          <li>{obj.region + " " + obj.distribution}</li>
+        ))}
+      </ul>
+      <svg
+          ref={ref}
+      />
+    </>
+  )
+}
+
+export async function getStaticProps(context) {
+  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0'
+
+  const res = await fetch(`https://localhost/land_values/distributionRegion/2021`)
+  const donut_data = await res.json()
+
+  if (!donut_data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { donut_data }, 
+  }
+}
+
+export default Donut_chart;
