abdfb6782d7c6e29a935dd3ea15e1ffc5774e6ba
Niels Kerné <365-kn175318@users.noreply.www-apps.univ-lehavre.fr>
2021-12-22 23:36:03 +0100

    création fonction de crétion et d'affichage des axes

--- pwa/pages/PrixMetre2ParAnnee.tsx
+++ pwa/pages/PrixMetre2ParAnnee.tsx
@@ -16,29 +16,32 @@ export default function PrixMetre2ParAnneeComponnent() {
             const svg = d3.select('svg')
                 .attr("width", width)
                 .attr("height", heigh);
-            var scale = d3.scaleLinear()
-                .domain([2017, 2019])
-                .range([0, width * 3/4]);
-            var x_axis = d3.axisBottom(scale)
-                .scale(scale)
-                .tickValues([2017, 2018, 2019]);
-            var scale = d3.scaleLinear()
-                .domain([2, 4])
-                .range([heigh/2, 0]);
-            var y_axis = d3.axisLeft(scale)
-                .scale(scale)
-                .tickValues([2, 3, 4]);
-            svg.append("g")
-                .attr("transform", "translate(" + coordonneesBaseTransitionx + ", " + (heigh/2 + coordonneesBaseTransitiony) + ")")
-                .call(x_axis);
-            svg.append("g")
-                .attr("transform", "translate(" + coordonneesBaseTransitionx + ", " + coordonneesBaseTransitiony + ")")
-                .call(y_axis);
+            creationAxis(d3, svg, width, heigh, coordonneesBaseTransitionx, coordonneesBaseTransitiony);
         };
         import_d3();
     }, [])
 
-//...
+    function creationAxis(d3, svg, width, heigh, coordonneesBaseTransitionx, coordonneesBaseTransitiony) {
+        var scale = d3.scaleLinear()
+            .domain([2017, 2019])
+            .range([0, width * 3/4]);
+        var x_axis = d3.axisBottom(scale)
+            .scale(scale)
+            .tickValues([2017, 2018, 2019]);
+        var scale = d3.scaleLinear()
+            .domain([2, 4])
+            .range([heigh/2, 0]);
+        var y_axis = d3.axisLeft(scale)
+            .scale(scale)
+            .tickValues([2, 3, 4]);
+        svg.append("g")
+            .attr("transform", "translate(" + coordonneesBaseTransitionx + ", " + (heigh/2 + coordonneesBaseTransitiony) + ")")
+            .call(x_axis);
+        svg.append("g")
+            .attr("transform", "translate(" + coordonneesBaseTransitionx + ", " + coordonneesBaseTransitiony + ")")
+            .call(y_axis);
+    }
+
     return (
         //...
         <svg></svg>
