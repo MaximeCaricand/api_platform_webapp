e81411898cc9aa553146cb1f71349a67aa6c4e50
Maxime Caricand <maxime.caricand@co-assist.fr>
2022-01-02 20:53:21 +0100

    rework landing page

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -3,7 +3,8 @@ const Index = () => (
     <div className="container py-5 h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <div className="col-12 p-5 text-center">
-          <h2 className="fw-bold mb-5">LANDING PAGE DU TURFU</h2>
+          <h2 className="fw-bold mb-5">AWESOME LANDING PAGE</h2>
+          <h4 className="fw-bold mb-5">PAGE D'ATTERRISSAGE IMPRESSIONNANTE</h4>
         </div>
       </div>
     </div>
